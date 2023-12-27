3e51eba5daa2f4e35fcf0319d775d5eac3bc75b5
Ali George <george7ali@gmail.com>
2023-12-27 02:40:24 +0100

    prevent the default action of button which is to try to send data to the backend

--- scroller.js
+++ scroller.js
@@ -2,7 +2,7 @@ function scroll() {
   const navLinks = document.querySelectorAll('a[href^="#"]');
   navLinks.forEach((anchor) => {
     anchor.addEventListener('click', (e) => {
-
+      e.preventDefault();
     });
   });
 }
