8b3133aee7dc74653bb11309355005a537ca5487
Ali George <george7ali@gmail.com>
2023-12-27 02:38:56 +0100

    target individual anchor elements and add a click event to it

--- scroller.js
+++ scroller.js
@@ -1,3 +1,8 @@
 function scroll() {
-    const navLinks = document.querySelectorAll('a[href^="#"]');
+  const navLinks = document.querySelectorAll('a[href^="#"]');
+  navLinks.forEach((anchor) => {
+    anchor.addEventListener('click', (e) => {
+
+    });
+  });
 }
