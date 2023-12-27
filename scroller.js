555f329a5b02e64a4caadf005ff9d8d1e5f55514
Ali George <george7ali@gmail.com>
2023-12-27 02:46:54 +0100

    get the href of each element and add event scroll into view and apply scroll behavior to smooth

--- scroller.js
+++ scroller.js
@@ -3,6 +3,9 @@ function scroll() {
   navLinks.forEach((anchor) => {
     anchor.addEventListener('click', (e) => {
       e.preventDefault();
+      element.(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth';
+      });
     });
   });
 }
