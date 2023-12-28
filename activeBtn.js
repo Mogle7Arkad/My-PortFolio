60dcded1b81b15fee146e067fcb068bd3df0a9be
Ali George <george7ali@gmail.com>
2023-12-28 12:53:21 +0100

    create a function and use for to loop through the buttons

--- activeBtn.js
+++ activeBtn.js
@@ -1,2 +1,7 @@
 const navLinks = document.getElementsByClassName('nav-link');
 
+function activeBtn(event) {
+  for (let i = 0; i < navLinks.length; i += 1) {
+    navLinks[i].classList.remove('active');
+  }
+}
