1dff3d362b9781d0a7c21d16dc38c71cf6efb812
Ali George <george7ali@gmail.com>
2023-12-28 11:40:25 +0100

    restructure the code to effect parsing error dictated by eslint

--- scroller.js
+++ scroller.js
@@ -1,11 +1,18 @@
 function scroll() {
-  const navLinks = document.querySelectorAll('a[href^="#"]');
-  navLinks.forEach((anchor) => {
-    anchor.addEventListener('click', (e) => {
-      e.preventDefault();
-      element.(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth';
-      });
+  document.querySelectorAll('a[href^="#"]').forEach((element) => {
+    element.addEventListener('click', (event) => {
+      event.preventDefault();
+      const targetHref = event.currentTarget.getAttribute('href');
+
+      if (targetHref) {
+        const targetElement = document.querySelector(targetHref);
+
+        if (targetElement) {
+          targetElement.scrollIntoView({
+            behavior: 'smooth',
+          });
+        }
+      }
     });
   });
 }
