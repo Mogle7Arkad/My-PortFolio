2856b3bd7f548fa8f05bb0e72187c1c3c4700950
Ali George <george7ali@gmail.com>
2024-01-08 00:24:39 +0100

    add eventlistener to the button and prevent default to stop the button to try to send information to backend

--- src/contactMe.js
+++ src/contactMe.js
@@ -3,7 +3,9 @@ const contactLink = document.querySelector('.contact-me');
 const button = document.querySelector('.btn');
 
 function displayContactLinks() {
-
+  button.addEventListener('click', (e) => {
+    e.preventDefault();
+  });
 }
 
 export default displayContactLinks;
