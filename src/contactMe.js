8c9a5a89a7b6e748d430d0325dd37767aef2dec0
Ali George <george7ali@gmail.com>
2024-01-08 00:23:26 +0100

    create the function to display the links and export it

--- src/contactMe.js
+++ src/contactMe.js
@@ -1,3 +1,9 @@
 
 const contactLink = document.querySelector('.contact-me');
 const button = document.querySelector('.btn');
+
+function displayContactLinks() {
+
+}
+
+export default displayContactLinks;
