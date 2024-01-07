7776899a25cb07a5476c99cd0d13520c742cd1d4
Ali George <george7ali@gmail.com>
2024-01-08 00:29:40 +0100

    import the function displayContactLink

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import activateHamburger from './hamburger.js';
 import scroll from '../scroller.js';
 import handleClick from '../activeBtn.js';
+import displayContactLinks from './contactMe.js';
 
 activateHamburger();
 handleClick();
 scroll();
+displayContactLinks();
