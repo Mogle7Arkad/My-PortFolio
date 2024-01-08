e9e63fb9a6d483ba9c904333692f5c53e5379528
Ali George <george7ali@gmail.com>
2024-01-08 02:47:12 +0100

    import the close drop down function and called it to implement it

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import activateHamburger from './hamburger.js';
+import { activateHamburger, closeDropDown } from './hamburger.js';
 import scroll from '../scroller.js';
 import handleClick from '../activeBtn.js';
 import displayContactLinks from './contactMe.js';
 
 activateHamburger();
+closeDropDown();
 handleClick();
 scroll();
 displayContactLinks();
