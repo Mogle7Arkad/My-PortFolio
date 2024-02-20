ea3c43a6af6b007309fa7179768e28b07c93d203
Ali George <george7ali@gmail.com>
2024-02-20 15:49:17 +0100

    run build to compile and update main

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import { activateHamburger, closeDropDown } from './hamburger.js';
 import scroll from '../scroller.js';
 import handleClick from '../activeBtn.js';
-import displayContactLinks from './contactMe.js';
 
 activateHamburger();
 closeDropDown();
